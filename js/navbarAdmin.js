2fadab6ea5fdfe6c91be4bddbc8f90c7cb24cfe8
Justo Cano <ju.cano@duocuc.cl>
2023-06-08 10:53:03 -0400

    Fue agregado la pagina web

--- js/navbarAdmin.js
+++
@@ -1,9 +0,0 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
-        document.getElementById('menu-admin').outerHTML
-            =this.responseText;
-    }
-}
-xhttp.open('GET' , 'navegadorAdmin.html' ,true);
-xhttp.send();
--- js/navbarCli.js
+++
@@ -1,9 +0,0 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
-        document.getElementById('menu-cliente').outerHTML
-            =this.responseText;
-    }
-}
-xhttp.open('GET' , 'navegadorCli.html' ,true);
-xhttp.send();
